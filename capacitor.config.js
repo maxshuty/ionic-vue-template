c586890e4a2440eab7354fb4d38b65cc95d5f345
Max Poshusta <MaxPoshusta@gmail.com>
2025-01-21 07:00:40 -0600

    Basic app conversion from ts to js

---
+++ capacitor.config.js
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-vue-template',
+  webDir: 'dist',
+};
+
+export default config;
